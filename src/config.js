9c5671fb143fac2266743ee2867d2d128b8a5923
Daniel Fong <daniel.fong.mv@gmail.com>
2020-09-26 12:50:54 -0600

    added db url and jwt info to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/mtb-trails',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/mtb-trails-test',
+    JWT_SECRET: process.env.JWT_SECRET || 'its a secret',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   }
