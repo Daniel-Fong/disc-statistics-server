38fc121ad0f49a00070afeb9da6563579c9b38fd
Daniel Fong <daniel.fong.mv@gmail.com>
2020-10-27 13:09:52 -0600

    added wind, direction, and disc selection

--- migrations/008.do.create_strokes.sql
+++ migrations/008.do.create_strokes.sql
@@ -3,7 +3,10 @@ CREATE TABLE strokes (
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
     hole_id INTEGER REFERENCES holes(id) ON DELETE CASCADE NOT NULL,
     date_modified TIMESTAMPTZ DEFAULT now() NOT NULL,
+    disc_id INTEGER REFERENCES discs(id),
     shot_type TEXT NOT NULL;
     shot_result TEXT NOT NULL;
+    wind TEXT,
+    wind_direction TEXT,
     notes TEXT
 );
