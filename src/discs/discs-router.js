c0a3ec52968bda68695d09716b2d4740b2b6e9b0
Daniel Fong <daniel.fong.mv@gmail.com>
2020-11-09 17:14:58 -0700

    added some auth to disc router;

--- src/discs/discs-router.js
+++ src/discs/discs-router.js
@@ -26,6 +26,72 @@ const serializeDisc = disc => ({
     thrown,
     date_modified,
     notes: xss(disc.notes)
-})
+});
+
+discsRouter
+    .route('/')
+    .post(requireAuth, async (req, res, next) => {
+        const {name, brand, mold, type, plastic, stability, primaryColor, secondaryColor, speed, glide, turn, fade, notes} = req.body;
+        const user_id = req.user_id;
+        const newDisc = {
+            user_id,
+            name,
+            brand,
+            mold,
+            type,
+            plastic,
+            stability,
+            primaryColor,
+            secondaryColor,
+            speed,
+            glide,
+            turn,
+            fade,
+            notes
+        };
+        if (!name) {
+            return res.status(400).json({ error: { message: 'name required' } });
+          }
+          if (!brand) {
+            return res.status(400).json({ error: { message: 'brand required' } });
+          }
+          if (!mold) {
+            return res
+              .status(400).json({ error: { message: 'mold required' } });
+          }
+          if (!plastic) {
+            return res
+              .status(400).json({ error: { message: 'plastic required' } });
+          }
+          if (name.length > 30) {
+            return res
+              .status(400)
+              .json({
+                error: { message: 'Disc name must not exceed 30 characters' },
+              });
+          }
+          if (brand.length > 20) {
+            return res
+              .status(400)
+              .json({ error: { message: 'Disc brand must not exceed 20 characters' } });
+          }
+          if (mold.length > 20) {
+            return res
+              .status(400)
+              .json({ error: { message: 'Disc mold must not exceed 20 characters' } });
+          }
+          try {
+            const disc = await DiscsService.insertDisc(
+              req.app.get('db'), 
+              newDisc
+            );
+            res
+              .status(201)
+              .location(path.posix.join(req.originalUrl, `${post.id}`))
+              .json(serializePost(post));
+          } catch (error) {
+            next(error)
+          }
+    })
 
 module.exports = discsRouter;
